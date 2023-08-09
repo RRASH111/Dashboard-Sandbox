cec8d80fb183beae7c8c0e30586b5601c426c263
RRASH111 <ranishwaiki3@gmail.com>
2023-08-09 14:25:52 +0300

    file structure
    i forgot to push yesterday Im a good dev :)

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Import from 'react-dom', not 'react'
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
